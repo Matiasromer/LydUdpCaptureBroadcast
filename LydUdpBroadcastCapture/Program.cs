3a1e9369598918972c7a6d08ac4ea8fbc6efeb2b
Matias Alexander Rømer <mati0681@edu.easj.dk>
2017-11-28 15:25:31 +0100

    tilføjet metode til at starte og stoppe program

--- LydUdpBroadcastCapture/Program.cs
+++ LydUdpBroadcastCapture/Program.cs
@@ -16,8 +16,8 @@ namespace LydUdpBroadcastCapture
         // Virker for Soap
         static void Main(string[] args)
         {
-            
-            
+
+
             Start();
             Console.ReadKey();
         }
@@ -34,21 +34,59 @@ namespace LydUdpBroadcastCapture
                     IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
                     while (true)
                     {
-
+                        
                         Console.WriteLine("Waiting for broadcast {0}", client.Client.LocalEndPoint);
                         byte[] datagramReceived = client.Receive(ref remoteEndPoint);
 
                         string str = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
                         Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
                             remoteEndPoint.Address, remoteEndPoint.Port, str);
-                        
+
+                        if (reciever.TjekStatus())
+                        {
+                            reciever.PostLydToList(str);
+                            Console.WriteLine("Saved");
+                        }
 
 
-                        reciever.PostLydToList(str);
                     }
                 }
             }
         }
+
+        //private static void Start()
+        //{
+
+        //    using (var reciever = new ServiceReferenceLocalhost.Service1Client())
+        //    {
+
+
+        //        using (UdpClient client = new UdpClient(new IPEndPoint(IPAddress.Any, port)))
+        //        {
+
+        //            IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
+        //            while (true)
+        //            {
+
+        //                Console.WriteLine("Waiting for broadcast {0}", client.Client.LocalEndPoint);
+        //                byte[] datagramReceived = client.Receive(ref remoteEndPoint);
+
+        //                string str = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+        //                Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
+        //                    remoteEndPoint.Address, remoteEndPoint.Port, str);
+
+        //                if (reciever.TjekStatus())
+        //                {
+        //                    reciever.PostLydToList(str);
+        //                    Console.WriteLine("Saved");
+        //                }
+
+
+        //            }
+        //        }
+        //    }
+        //}
+
         private static void Parse(string response)
         {
             string[] parts = response.Split(' ');
@@ -64,5 +102,5 @@ namespace LydUdpBroadcastCapture
 
 
 }
-                
+
 
